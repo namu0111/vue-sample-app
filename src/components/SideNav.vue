4a26b080668fe0a9b9eaa1a80ec3e74bcf55c048
dx <trailhead.practice@gmail.com>
2021-03-21 05:03:25 +0900

    add: SIdeNav に tooltip を作成

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -5,16 +5,21 @@
         clipped
         absolute
     >
+
         <v-list dense>
             <v-list-item
                 v-for="item in items"
                 :key="item.title"
                 :to="item.link"
             >
-                <v-list-item-icon>
-                <v-icon>{{ item.icon }}</v-icon>
-                </v-list-item-icon>
-
+                <v-tooltip right>
+                  <span>{{ item.title }}</span>
+                  <template v-slot:activator="{ on, attrs }">           
+                    <v-list-item-icon v-on="on" v-bind="attrs">
+                    <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon> 
+                  </template>
+                </v-tooltip>
                 <v-list-item-content>
                 <v-list-item-title>{{ item.title }}</v-list-item-title>
                 </v-list-item-content>
@@ -29,8 +34,8 @@
       return {
         items: [
           { title: 'Home', icon: 'mdi-home-city', link: { name: 'home'} },
-          { title: 'My Account', icon: 'mdi-account', link: { name: 'addresses' } },
-          { title: 'Users', icon: 'mdi-account-group-outline' },
+          { title: 'My Account', icon: 'mdi-card-account-mail-outline', link: { name: 'addresses' } },
+          { title: 'Profile', icon: 'mdi-account' },
         ],
       }
     },
