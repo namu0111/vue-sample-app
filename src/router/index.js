7a6bc8f1816f85d9f9807a5fe8d5d4c1666d3115
dx <trailhead.practice@gmail.com>
2021-03-21 05:03:06 +0900

    add: Login 画面作成、Login リダイレクトルート作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 import Addresses from '../views/Addresses.vue'
 import AddressForm from '../views/AddressForm.vue'
+import firebase from 'firebase'
 
 Vue.use(VueRouter)
 
@@ -10,7 +12,8 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    meta: { requiresAuth: true },
   },
   {
     path: '/about',
@@ -18,17 +21,25 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/addresses',
     name: 'addresses',
-    component: Addresses
+    component: Addresses,
+    meta: { requiresAuth: true },
   },
   {
     path: '/addresses/:address_id?/edit',
     name: 'address_edit',
-    component: AddressForm
+    component: AddressForm,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
 ]
 
@@ -36,6 +47,23 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if (requiresAuth) {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        next()
+      } else {
+        next({
+          name: 'Login',
+        })
+      }
+    })
+  } else {
+    next() // next() を常に呼び出すようにしてください!
+  }
 })
 
 export default router
