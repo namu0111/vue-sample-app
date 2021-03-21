44323264a71f9ac8ab4c1a6cefaf9bec14fe12c9
dx <trailhead.practice@gmail.com>
2021-03-22 05:07:02 +0900

    update: auth0 でのログインに変更

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,19 @@
         color="primary"
         dark
     >
-        <v-app-bar-nav-icon @click.stop="toggleSideMenu" v-show='$store.state.login_user'></v-app-bar-nav-icon>
-        <v-toolbar-title @click="$router.push({ name: 'home' })">マイアドレス帳</v-toolbar-title>
+        <v-app-bar-nav-icon @click.stop="toggleSideMenu" v-show='$auth.isAuthenticated'></v-app-bar-nav-icon>
+        <v-toolbar-title @click="$router.push({ name: 'home' })">Contact List App</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-          <v-btn text @click="logout" v-if='$store.state.login_user'>ログアウト</v-btn>
+          <!-- show login when not authenticated -->
+          <v-btn text @click="login" v-if="!$auth.isAuthenticated">Auth0 Log in</v-btn>
+          <v-btn text v-if="$auth.isAuthenticated" link :to="{ name: 'profile'}" >{{ $store.getters.userName }}</v-btn>
+          <!-- show logout when authenticated -->
+          <v-btn text @click="logout" v-if="$auth.isAuthenticated">Auth0 Log out</v-btn>
         </v-toolbar-items>
     </v-app-bar>
     <v-content>
-      <v-container v-show='$store.state.login_user'>
+      <v-container v-show='$auth.isAuthenticated'>
        <SideNav/>
       </v-container>
       <v-container align-start>
@@ -26,7 +30,6 @@
 <script>
 import { mapActions, mapGetters } from 'vuex'
 import SideNav from './components/SideNav.vue';
-import firebase from 'firebase'
 
 export default {
   name: 'App',
@@ -34,31 +37,34 @@ export default {
   components: {
     SideNav,
   },
-  computed: {
-    ...mapGetters(['userName'])
+  beforeUpdate () {
+    this.setAuth0LoginUser();
   },
-  created () {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setLoginUser(user)
-        this.fetchAddresses()
-        if (this.$router.currentRoute.name === 'NoAuth') this.$router.push({ name: 'home' })
-      }else{
-        this.deleteLoginUser()
-        this.$router.push({ name: 'NoAuth' })
-      }
-    }
-    )
+  computed: {
+    ...mapGetters(['userName',])
   },
   data: () => ({
     //
   }),
   methods: {
+    async login() {
+      await this.$auth.loginWithRedirect();
+    },
+    async logout() {
+      await this.$auth.logout({
+        returnTo: window.location.origin  
+      },);
+      console.log('Log out')
+    },
+    async setAuth0LoginUser(){
+        await this.setLoginUser(this.$auth.user)
+        console.log(this.$auth.user)
+        console.log(this.$store.getters.user)
+    },
     ...mapActions(
       [
         'toggleSideMenu',
         'setLoginUser',
-        'logout',
         'deleteLoginUser',
         'fetchAddresses',
       ]
