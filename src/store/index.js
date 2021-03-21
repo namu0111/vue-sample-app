1bbd9cd23ed0dc9beaf17ed2412d21726ee50d28
dx <trailhead.practice@gmail.com>
2021-03-21 18:06:46 +0900

    add: データ更新処理を追加

--- src/store/index.js
+++ src/store/index.js
@@ -25,6 +25,10 @@ export default new Vuex.Store({
       address.id = id
       state.addresses.push(address)
     },
+    updateAddress (state, { id, address }) {
+      const index = state.addresses.findIndex(address => address.id === id)
+      state.addresses[index] = address
+    },
   },
   actions: {
     setLoginUser ({ commit }, user) {
@@ -53,6 +57,13 @@ export default new Vuex.Store({
       if (getters.uid) firebase.firestore().collection(`users/${getters.uid}/addresses`).add(address).then(doc => {
         commit('addAddress', { id: doc.id, address} )
       })
+    },
+    updateAddress ({ getters, commit }, { id, address }) {
+      if (getters.uid) {
+        firebase.firestore().collection(`users/${getters.uid}/addresses`).doc(id).update(address).then(() => {
+          commit('updateAddress', { id, address })
+        })
+      }
     },
   },
   getters: {
