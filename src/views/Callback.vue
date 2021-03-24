773fefd8ce9f2ae4f5e022916220a4711196e2a5
daiki.nakamura <trailhead.practice@gmail.com>
2021-03-25 02:21:52 +0900

    add: callback page

--- src/views/Callback.vue
+++ src/views/Callback.vue
@@ -1,7 +1,28 @@
 <template>
-  <v-row justify="center">
-    <v-col md="auto">
-      <div>Trying to callback</div>
-    </v-col>
-  </v-row>
+  <div class="spinner">
+    <img src="../assets/loading.svg" alt="Loading">
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    
+  },
+};
+</script>
+
+<style scoped>
+.spinner {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: white;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
